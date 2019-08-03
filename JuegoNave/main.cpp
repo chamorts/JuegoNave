904543e130f0a231dff7a26519a821f177c20d0c
chamorts <chamo_rts@hotmail.com>
2019-08-03 08:07:15 -0400

    Tercer Commit Limites/Salud
    algunos errores en los caracteres de corazones

--- JuegoNave/main.cpp
+++ JuegoNave/main.cpp
@@ -23,16 +23,34 @@ void OcultalCursor(){
 
 }
 
+void pintar_limites(){
+    for(int i = 2 ; i<78 ; i++){
+        gotoxy(i,3); printf("%c",205);
+        gotoxy(i,33); printf("%c",205);
+    }
+
+    for(int i = 4 ; i < 33 ; i++){
+        gotoxy(2,i); printf("%c",186);
+        gotoxy(77,i); printf("%c",186);
+    }
+
+    gotoxy(2,3); printf("%c",201);
+    gotoxy(2,33); printf("%c",200);
+    gotoxy(77,3); printf("%c",187);
+    gotoxy(77,33); printf("%c",188);
+
+}
+
 class NAVE{
     int x,y;
+    int corazones;
 public:
-    NAVE(int _x, int _y): x(_x),y(_y){}
+    NAVE(int _x, int _y, int _corazones): x(_x),y(_y),corazones(_corazones){}
     void pintar();
     void borrar();
     void mover();
+    void pintar_corazones();
 };
-
-
 
 void NAVE::pintar(){
     gotoxy(x,y); printf("  %c",30);
@@ -53,21 +71,37 @@ void NAVE::mover(){
     if(kbhit()){
             char tecla = getch();
             borrar();
-            if(tecla== 'a')x--;
-            if(tecla== 'd')x++;
-            if(tecla== 'w')y--;
-            if(tecla== 's')y++;
+            if(tecla == 'a' && x > 3) x--;
+            if(tecla == 'd'&& x+6 < 77) x++;
+            if(tecla == 'w' && y > 4) y--;
+            if(tecla == 's' &&y+3 < 33) y++;
+            //if(tecla == 'e')corazones--;
             pintar();
+            pintar_corazones();
         }
+
+}
+
+void NAVE::pintar_corazones(){
+    gotoxy(64,2); printf("Salud");
+    gotoxy(70,2); printf("     ");
+    for(int i = 0; i < corazones; i++){
+
+        gotoxy(70+i,2); printf("%c",3);
+    }
 
 
 }
 
+
 int main(){
 
    OcultalCursor();
-   NAVE N(7,7);
+   pintar_limites();
+   NAVE N(7,7,3);
    N.pintar();
+   N.pintar_corazones();
+
    bool game_over=false;
    while(!game_over){
 
